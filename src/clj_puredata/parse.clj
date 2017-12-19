17841119f64b2c71d65f10f96c33025e5a7ee6d4
Philipp Dikmann <mail@philippdikmann.de>
2017-12-19 23:40:18 +0100

    remove cruft from parser sources.

--- src/clj_puredata/parse.clj
+++ src/clj_puredata/parse.clj
@@ -1,45 +1,6 @@
 (ns clj-puredata.parse
   (:require [clojure.test :as t]))
 
-(defn parse-old
-  [form]
-  (cond
-    (and (vector? form)
-         (keyword? (first form)))
-    (let [op (first form)
-          [options args] (if (map? (second form))
-                           [(second form) (drop 2 form)]
-                           [{} (rest form)])]
-      {:op (subs (str (first form)) 1)
-       :options options
-       :args (mapv parse-old args)})
-    ;;
-    (number? form)
-    form
-    ;;
-    (string? form)
-    form
-    ;;
-    :else
-    (throw (Exception. "Unknown input to clj-puredata.parse-old"))))
-
-(t/deftest parse-old-test
-  (t/testing "simple forms"
-    (t/is (= (parse-old [:+ 1 2])
-             {:op "+"
-              :options {}
-              :args [1 2]})))
-  (t/testing "recursion"
-    (t/is (= (parse-old [:+ [:* 2 2] 1])
-             {:op "+"
-              :options {}
-              :args [{:op "*"
-                      :options {}
-                      :args [2 2]}
-                     1]}))))
-
-;; --------------------------------------------------------------------------------
-
 (def parse-context (atom nil))
 
 (defn setup-parse-context []
@@ -82,89 +43,14 @@
   (and (map? arg)
        (= (:type arg) :node)))
 
-(declare parse-element)
-
-(defn outlet-old [node n]
-  "assoc an :outlet key into a node. this only touches nodes as they
-  are passed on through #'parse-element / #'recur-on-node-args and
-  does not mutate the node stored inside the patch."
-  #(assoc (parse-element node) :outlet n))
-
-(defn inlet-old [node n]
-  "assoc an :inlet key into a node. this only touches nodes as they
-  are passed on through #'parse-element / #'recur-on-node-args and
-  does not mutate the node stored inside the patch."
-  #(assoc (parse-element node) :inlet n))
-
-(defn recur-on-node-args [args id inlet & {:keys [acc] :or {acc []}}]
-  "Makes sure that literal arguments (to nodes) are passed verbatim
-  while those of type :node, :outlet and :inlet will create a new
-  connection to the correct inlet."
-  (if (empty? args)
-    acc
-    (let [arg (parse-element (first args))]
-      (cond
-        (node? arg)
-        (do (add-element {:type :connection
-                          :from-node {:id (:id arg)
-                                      :outlet (:outlet arg 0)} ; specified by #'outlet wrapper (of source), or default outlet (0)
-                          :to-node {:id id
-                                    :inlet (:inlet arg inlet)}}) ; specified by #'inlet wrapper (of source), or current inlet.
-            (recur-on-node-args (rest args)
-                                id
-                                (inc inlet)
-                                :acc acc))
-        ;;
-        (nil? arg)             ; explicit NIL argument skips an inlet.
-        (recur-on-node-args (rest args)
-                            id
-                            (inc inlet)
-                            :acc acc)
-        ;;
-        :else                 ; literals are added to argument vector.
-        (recur-on-node-args (rest args)
-                            id
-                            inlet
-                            :acc (conj acc arg))))))
-
-(defn parse-element [form]
-  (cond
-    (hiccup? form) ;; hiccup syntax detected -> create new node
-    (let [op (first form)
-          [options args] (if (map? (second form))
-                           [(second form) (drop 2 form)]
-                           [{} (rest form)])
-          op (op-from-kw (first form))
-          id (dispense-node-id)
-          parsed-args (recur-on-node-args args id 0)
-          node {:type :node :op op :id id :options options :args parsed-args}]
-      (add-element node))
-    ;;
-    (literal? form)
-    form
-    ;;
-    (fn? form)
-    (form)))
-
 (defn sort-patch [patch]
   (->> patch
        (sort-by :id)
        (sort-by (comp :id :from-node))
        vec))
 
-(defn parse [form]
-  "A macro that delays any evaluations until the patch context is setup."
-  (setup-parse-context)
-  (parse-element form)
-  (let [patch (sort-patch (:patch @parse-context))]
-    (teardown-parse-context)
-    patch))
-
-;; --------------------------------------------------------------------------------
-
 (defn processed? [node]
   ((:processed-node-ids @parse-context) (:id node)))
-
 
 (defn outlet [node n]
   (assoc node :outlet n))
--- test/clj_puredata/parse_test.clj
+++ test/clj_puredata/parse_test.clj
@@ -2,69 +2,6 @@
   (:require [clj-puredata.parse :refer :all]
             [clojure.test :refer :all]))
 
-(deftest basic
-  (testing "Parsing"
-    (testing "a simple form."
-      (is (= (parse [:+ 1 2])
-             [{:type :node :op "+" :id 0
-               :options {} :args [1 2]}])))
-    (testing "recursively, which triggers connections."
-      (is (= (parse [:+ [:* 2 2] 1])
-             [{:type :node :op "+" :id 0
-               :options {} :args [1]}
-              {:type :node :op "*" :id 1
-               :options {} :args [2 2]}
-              {:type :connection
-               :from-node {:id 1 :outlet 0}
-               :to-node {:id 0 :inlet 0}}])))
-    (testing "will skip target inlet when argument is NIL."
-      (is (= (parse [:+ nil [:*]])
-             [{:type :node :op "+" :id 0
-               :options {} :args []}
-              {:type :node :op "*" :id 1
-               :options {} :args []}
-              {:type :connection
-               :from-node {:id 1 :outlet 0}
-               :to-node {:id 0 :inlet 1}}])))
-    (testing "can adjust target inlet."
-      (is (= (parse [:+ (inlet-old [:*] 1)])
-             [{:type :node :op "+" :id 0
-               :options {} :args []}
-              {:type :node :op "*" :id 1
-               :options {} :args []}
-              {:type :connection
-               :from-node {:id 1 :outlet 0}
-               :to-node {:id 0 :inlet 1}}])))
-    (testing "can adjust source outlet."
-      (is (= (parse [:+ (outlet-old [:*] 1)])
-             [{:type :node :op "+" :id 0
-               :options {} :args []}
-              {:type :node :op "*" :id 1
-               :options {} :args []}
-              {:type :connection
-               :from-node {:id 1 :outlet 1}
-               :to-node {:id 0 :inlet 0}}])))))
-
-#_(deftest tricky
-    (testing "Tricky parsing"
-      (testing "accomodates the use of LET."
-        (is (= (parse (let [x [:* 2 2]] [:+ x x]))
-               [{:type :node :op "+" :id 0
-                 :options {} :args []}
-                {:type :node :op "*" :id 1
-                 :options {} :args [2 2]}
-                {:type :connection
-                 :from-node {:id 1 :outlet 0}
-                 :to-node {:id 0 :inlet 0}}
-                {:type :connection
-                 :from-node {:id 1 :outlet 0}
-                 :to-node {:id 0 :inlet 1}}])
-            "This might be a bit motivated - does this require a macro that walks the form, distinguishing hiccup and regular clojure? ")
-        #_(is (= (pd-patch
-                  (let [x (pd [:* 2 2])]
-                    (pd [:+ x x]))))
-              "This seems more practical - #'pd returns a map (still using unique ids for nodes), constructs a tree with duplicates, and sort them out later."))))
-
 (deftest constructing-the-tree
   (testing "The function PD"
     (testing "will expand hiccup [:+ 1 2] to maps {:type ::node ...}."
@@ -116,41 +53,15 @@
     (testing "writes nodes out into the :PATCH field of atom PARSE-CONTEXT."
       (is (= (-> (with-patch (pd [:+]) (pd [:-]))
                  :patch count)
-             2)
-          #_(= (:patch (with-patch (pd [:+]) (pd [:-])))
-               [{:type :node
-                 :op "+" :id 0
-                 :options {} :args []}
-                {:type :node
-                 :op "-" :id 1
-                 :options {} :args []}])))
+             2)))
     (testing "creates connections when nodes have other nodes as arguments."
       (is (= (-> (:patch (with-patch (pd [:+ [:-]])))
                  (nth 2) :type)
-             :connection)
-          #_(= (:patch (with-patch (pd [:+ [:-]])))
-               [{:type :node
-                 :op "+" :id 0
-                 :options {} :args []}
-                {:type :node
-                 :op "-" :id 1
-                 :options {} :args []}
-                {:type :connection
-                 :from-node {:id 1, :outlet 0}
-                 :to-node {:id 0, :inlet 0}}])))
+             :connection)))
     (testing "skips inlet if NIL is supplied as an argument."
       (is (= (-> (:patch (with-patch (pd [:+ nil [:-]])))
                  (nth 2) :to-node :inlet)
-             1
-             #_[{:type :node
-                 :op "+" :id 0
-                 :options {} :args [nil]}
-                {:type :node
-                 :op "-" :id 1
-                 :options {} :args []}
-                {:type :connection
-                 :from-node {:id 1, :outlet 0}
-                 :to-node {:id 0, :inlet 1}}])))
+             1)))
     (testing "respects the keys set by #'OUTLET and #'INLET and modifies connections accordingly."
       (is (let [p (:patch (with-patch (pd [:+ (outlet (pd [:*]) 23) (inlet (pd [:/]) 42)])))]
             (and (= (-> p (nth 3) :from-node :outlet) 23)
