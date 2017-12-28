33109188a13807b6748e75368914c6f723cca7c3
Philipp Dikmann <mail@philippdikmann.de>
2017-12-28 20:17:41 +0100

    add all known 'object' nodes.

--- src/clj_puredata/translate.clj
+++ src/clj_puredata/translate.clj
@@ -1,7 +1,38 @@
 (ns clj-puredata.translate
   (:require [clojure.string :as string]))
 
-(def obj-nodes #{"+" "-" "*" "/"})
+(def obj-nodes #{;; -------- general --------
+                 "bang" "b" "float" "f" "symbol" "int" "i" "send" "s" "receive" "r" "select" "route" "pack" "unpack" "trigger" "t" "spigot" "moses" "until" "print" "makefilename" "change" "swap" "value" "v" "list"
+                 ;; -------- time --------
+                 "delay" "metro" "line" "timer" "cputime" "realtime" "pipe"
+                 ;; -------- math --------
+                 "+" "-" "*" "/" "pow" "==" "!=" ">" "<" ">=" "<=" "&" "&&" "||" "||||" "%" "<<" ">>" "mtof" "powtodb" "rmstodb" "ftom" "dbtopow" "dbtorms" "mod" "div" "sin" "cos" "tan" "atan" "atan2" "sqrt" "log" "exp" "abs" "random" "max" "min" "clip" "wrap"
+                 ;; -------- midi and osc --------
+                 "notein" "ctlin" "pgmin" "bendin" "touchin" "polytouchin" "midiin" "sysexin" "noteout" "ctlout" "pgmout" "bendout" "touchout" "polytouchout" "midiout" "makenote" "stripnote" "oscparse" "oscformat"
+                 ;; -------- arrays / tables --------
+                 "tabread" "tabread4" "tabwrite" "soundfiler" "table" "array"
+                 ;; -------- misc --------
+                 "loadbang" "serial" "netsend" "netreceive" "qlist" "textfile" "text" "openpanel" "savepanel" "bag" "poly" "key" "keyup" "keyname" "declare"
+                 ;; -------- audio math --------
+                 "+~" "-~" "*~" "/~" "max~" "min~" "clip~" "q8_rsqrt~" "q8_sqrt~" "sqrt~" "wrap~" "fft~" "ifft~" "rfft~" "rifft~" "pow~" "log~" "exp~" "abs~" "framp~" "mtof~" "ftom~" "rmstodb~" "dbtorms~"
+                 ;; -------- general audio manipulation --------
+                 "dac~" "adc~" "sig~" "line~" "vline~" "threshold~" "snapshot~" "vsnapshot~" "bang~" "samplerate~" "send~" "receive~" "throw~" "catch~" "block~" "switch~" "readsf~" "writesf~"
+                 ;; -------- audio oscillators and tables --------
+                 "phasor~" "cos~" "osc~" "tabwrite~" "tabplay~" "tabread~" "tabread4~" "tabosc4~" "tabsend~" "tabreceive~"
+                 ;; -------- audio filters --------
+                 "vcf~" "noise~" "env~" "hip~" "lop~" "bp~" "biquad~" "samphold~" "print~" "rpole~" "rzero~" "rzero_rev~" "cpole~" "czero~" "czero_rev~"
+                 ;; -------- audio delay --------
+                 "delwrite~" "delread~" "vd~"
+                 ;; -------- subwindows --------
+                 ;; pd
+                 "inlet" "outlet" "inlet~" "outlet~"
+                 ;; -------- data templates --------
+                 "struct" "drawcurve" "filledcurve" "drawpolygon" "filledpolygon" "plot" "drawnumber"
+                 ;; -------- accessing data --------
+                 "pointer" "get" "set" "element" "getsize" "setsize" "append" "scalar"
+                 ;; -------- extras --------
+                 "sigmund~" "bonk~" "choice" "hilbert~" "complex-mod~" "expr~" "expr" "fexpr~" "loop~" "lrshift~" "pd~" "rev1~" "rev2~" "rev3~" "bob~"})
+
 (def self-nodes #{"msg" "text"})
 
 (def default-options
@@ -38,7 +69,7 @@
 
 (defn translate-node [n]
   (loop [[k & rst] (keys node-templates)]
-    (cond (nil? k) nil
+    (cond (nil? k) (throw (Exception. (str "Not a valid node: " n)))
           (k (:op n)) (->> n
                            (merge-options (default-options k))
                            (fill-template (node-templates k)))
