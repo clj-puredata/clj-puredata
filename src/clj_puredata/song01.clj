b4b45740fa16d1c14d4052b13c3f130c0d7477de
Philipp Dikmann <mail@philippdikmann.de>
2018-01-01 19:27:40 +0100

    work on song.

--- src/clj_puredata/song01.clj
+++ src/clj_puredata/song01.clj
@@ -8,6 +8,7 @@
                                        other]]))
 
 (with-patch "counter.pd"
+  ^{:doc "Increments its output when banged. Starts at 0."}
   (pd (as-> [:+ 1 (other 'f)] %
         [:f {:name 'f} 0 [:b [:inlet]] %]
         [:outlet %])))
@@ -16,13 +17,47 @@
   (pd [:outlet ["counter.pd" [:metro "$1" [:loadbang]]]]))
 
 (with-patch "between.pd"
+  ^{:doc "Receives a number and outputs it when it is within [$1..$2[.
+Second outlet outputs 1 when number enters range and 0 when it exits range."}
   (pd (as-> [:inlet] %
         [:moses "$1" %]
-        [:moses "$2" (outlet 1 %)]
-        [:outlet %])))
+        [:moses {:name 'm} "$2" (outlet 1 %)]
+        [:t {:name 't} "f f" %]
+        [:outlet {:x 0} %])
+      [:outlet {:x 10}
+       [:change [:msg 1 (outlet 1 (other 't))]
+        (inlet 0 [:msg 0 (outlet 1 (other 'm))])]]))
+
+(with-patch "between-on-off.pd"
+  (pd [:outlet (outlet 1 ["between.pd" "$1" "$2" [:inlet]])]))
+
+(def notes [200 240 400 410])
+
+(defn sound1
+  [mod on off note]
+  (pd (as-> [:osc- (nth notes note)] %
+        (as-> (other 'beats) %2
+          [:mod mod %2]
+          [:msg "$1 20" ["between-on-off.pd" on off %2]]
+          [:line- %2]
+          [:*- % %2])
+        [:throw- "output" %])))
 
 (with-patch "song01.pd"
-  (pd [:print ["between.pd" 0 4 ["beats.pd" 125]]]))
+  ;; main bpm
+  (pd ["beats.pd" {:name 'beats} 125])
+  ;; output sum
+  (pd [:*- {:name 'dac} 0.1 [:catch- "output"]]
+      [:dac- (other 'dac) (other 'dac)])
+  ;; sound 1
+  (sound1 8 0 4 0)
+  (sound1 10 4 7 1)
+  (sound1 8 0 1 2)
+  (sound1 10 4 5 3)
+  ;; misc
+  (pd ["between.pd" {:name 'bt} 0 4 (other 'beats)]
+      [:print "count-between" (other 'bt)]
+      [:print "on-off-between" (outlet 1 (other 'bt))]))
 
 (defn startup
   []
