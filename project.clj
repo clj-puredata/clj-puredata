5a9a2a414e153749f20056725ba21a3284a78f60
Philipp Dikmann <mail@philippdikmann.de>
2018-01-14 17:14:33 +0100

    prep for distribution

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-puredata "0.1.0-SNAPSHOT"
   :description "generate PureData patches."
-  ;; :url "http://example.com/FIXME"
-  ;; :license {:name "Eclipse Public License"
-  ;;           :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/pdikmann/clj-puredata"
+  :license {:name "The MIT License (MIT)"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [overtone/osc-clj "0.9.0"]
                  [potemkin "0.4.4"]]
