71468bfbdec29bb80f169ec3d3053db6046c9e8f
Philipp Dikmann <mail@philippdikmann.de>
2021-03-11 20:24:14 +0100

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-puredata "0.2.0-SNAPSHOT"
+(defproject clj-puredata "0.2.0"
   :description "Generate PureData patches."
   :url "https://github.com/pdikmann/clj-puredata"
   :license {:name "The MIT License (MIT)"
